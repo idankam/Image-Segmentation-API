1dbaa50db407e2eac8fd7a169ade7e5c2a201569
idankam <idankam98@gmail.com>
2024-07-14 19:33:16 +0300

    API is ready

--- phase_3.py
+++ phase_3.py
@@ -34,6 +34,7 @@ class InferRequest:
                 self.model = SegmentationModelAI(torch_model, model_type='torch', input_size=(512, 512))
             except Exception as e:
                 print(f"Can't use torch model. Using Onnx instead. error: {e}")
+                self.infer_model_type = 'onnx'
                 onnx_model = ONNXModel.get_default_onnx_converted_segmentation_model()
                 self.model = SegmentationModelAI(onnx_model, model_type='onnx', input_size=(512, 512))
         else:
@@ -53,12 +54,10 @@ class InferRequest:
         return res
 
     def infer(self):
-        print("infer")
         logits, pixels_distribution, seg_map, _ = self.model(self.image_data)
         results = {'logits': logits.tolist(), 'pixels_distribution': pixels_distribution.tolist(),
                    'segmentation_map': seg_map.tolist()}
         self.infer_results = results
-        print(results)
 
     def get_requested_results(self):
 
@@ -80,50 +79,15 @@ class InferRequest:
 app = FastAPI()
 
 
-def infer(infer_model_type, image):
-    onnx_model = ONNXModel.get_default_onnx_converted_segmentation_model()
-    segmentation_model_onnx = SegmentationModelAI(onnx_model, model_type='onnx', input_size=(512, 512))
-    torch_model = TorchModel.get_default_torch_segmentation_model()
-    segmentation_model_torch = SegmentationModelAI(torch_model, model_type='torch', input_size=(512, 512))
-
-    if infer_model_type == "onnx":
-        logits, pixels_distribution, seg_map, _ = segmentation_model_onnx(image)
-        return logits.tolist(), pixels_distribution.tolist(), seg_map.tolist()
-    elif infer_model_type == "torch":
-        logits, pixels_distribution, seg_map, _ = segmentation_model_torch(image)
-        return logits.tolist(), pixels_distribution.tolist(), seg_map.tolist()
-    else:
-        raise ValueError(f"Model type '{infer_model_type}' is not registered.")
-
-
-def get_requested_results(requested_results, logits, pixels_distribution, seg_map):
-    # if not specified - return all result types
-    if requested_results is None or len(requested_results) == 0:
-        requested_results = ["logits", "pixels_distribution", "segmentation_map"]
-
-    # Prepare the results based on the requested results
-    inference_results = {}
-    for result_type in requested_results or []:
-        if result_type == "logits":
-            inference_results['logits'] = logits
-        elif result_type == "pixels_distribution":
-            inference_results['pixels_distribution'] = pixels_distribution
-        elif result_type == "segmentation_map":
-            inference_results['segmentation_map'] = seg_map
-        else:
-            inference_results[result_type] = "result type not supported"
-
-    return inference_results
-
-
 def perform_infer(infer_model_type, requested_results_types, image_type, image_data):
     request = InferRequest(infer_model_type, requested_results_types, image_type, image_data)
     request.set_SegmentationModelAI_instance()
     request.infer()
     inference_results = request.get_requested_results()
 
-    return {"infer_model_type": infer_model_type,
-            image_type: image_data,
+    return {"Status": "OK",
+            "infer_model_type": request.infer_model_type,
+            image_type: request.image_type,
             "requested_results": inference_results}
 
 
@@ -157,30 +121,12 @@ async def infer_from_file(
         file: UploadFile = File(...),
 ):
     try:
-        # Read image file asynchronously
         image_content = await file.read()
-
-        # # Perform inference
-        # # logits, pixels_distribution, seg_map = infer(infer_model_type, contents)
-        # # inference_results = get_requested_results(requested_results, logits, pixels_distribution, seg_map)
-        #
-        # request = InferRequest(infer_model_type, requested_results_types, 'file', image_content)
-        # request.infer()
-        # inference_results = request.get_requested_results()
-        #
-        # # # Save the image (for demonstration)
-        # # image.save(f"uploaded_{file.filename}")
-        #
-        # # Return details
-        # return {
-        #     "infer_model_type": infer_model_type,
-        #     "filename": file.filename,
-        #     "requested_results": inference_results,
-        # }
         return perform_infer(infer_model_type, requested_results_types, 'file', image_content)
-
+    except ValueError as e:
+        raise HTTPException(status_code=401, detail=f"Fail! Value error: {str(e)}")
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail=f"Fail! Error: {str(e)}")
 
 
 if __name__ == "__main__":
